56c3c76fea074e53f46356c3162bea2de9c88bbe
ninja <astore.a.fabio@gmail.com>
2025-09-21 12:03:07 -0300

    cpf

--- br_docs/cpf.py
+++ br_docs/cpf.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+from random import randint
+from typing import Annotated, Generator, AsyncGenerator
 
 from pydantic import AfterValidator
 
@@ -34,58 +35,25 @@ def remove_mask(cpf: str) -> str:
     return new_cpf
 
 
-def __is_first_verificator_digit_valid(cpf: str) -> bool:
-    result = 0
-    sequence = 10
-    for digit in cpf[:-2:]:
-        result += int(digit) * sequence
-        sequence -= 1
-    result = (result * 10) % 11
-    if result == 10:
-        result = 0
-    return str(result) == cpf[-2]
-
-
-def is_first_verificator_digit_valid(cpf: str) -> bool:
-    cpf = remove_mask(cpf)
-    return __is_first_verificator_digit_valid(cpf)
-
-
-def validate_first_verificator_digit(cpf: str) -> str:
-    if not is_first_verificator_digit_valid(cpf):
-        raise "Invalid first verificator digit"
-    return cpf
-
-
-def __is_second_verificator_digit_valid(cpf: str) -> bool:
-    sequence = 11
-    result = 0
-    for digit in cpf[:-1:]:
-        result += int(digit) * sequence
-        sequence -= 1
-    result = (result * 10) % 11
-    if result == 10:
-        result = 0
-    return str(result) == cpf[-1]
-
-
-def is_second_verificator_digit_valid(cpf: str) -> bool:
-    cpf = remove_mask(cpf)
-    return __is_second_verificator_digit_valid(cpf)
-
-
-def validate_second_verificator_digit(cpf: str) -> str:
-    if not is_second_verificator_digit_valid(cpf):
-        raise "Invalid second verificator digit"
-    return cpf
-
-
 def __is_valid(cpf: str) -> bool:
     if len(cpf) != 11 or cpf.count(cpf[0]) == 11:
         return False
-    return __is_first_verificator_digit_valid(
-        cpf
-    ) and __is_second_verificator_digit_valid(cpf)
+    first_result = 0
+    second_result = 0
+    sequence = 10
+    for digit in cpf[:-2:]:
+        digit = int(digit)
+        first_result += digit * sequence
+        second_result += digit * (sequence + 1)
+        sequence -= 1
+    second_result += int(cpf[-2]) * 2
+    first_result = (first_result * 10) % 11
+    if first_result == 10:
+        first_result = 0
+    second_result = (second_result * 10) % 11
+    if second_result == 10:
+        second_result = 0
+    return str(first_result) == cpf[-2] and str(second_result) == cpf[-1]
 
 
 def is_valid(cpf: str) -> bool:
@@ -95,13 +63,49 @@ def is_valid(cpf: str) -> bool:
 
 def validate(cpf: str) -> str:
     cpf = remove_mask(cpf)
-    if not __is_valid(cpf):
-        raise "Invalid CPF"
+    if __is_valid(cpf):
+        return cpf
+    raise "Invalid CPF"
+
+
+def __generate_verificator_digits(cpf) -> str:
+    first_result = 0
+    second_result = 0
+    sequence = 10
+    for digit in cpf:
+        first_result += int(digit) * sequence
+        second_result += int(digit) * (sequence + 1)
+        sequence -= 1
+    first_result = (first_result * 10) % 11
+    if first_result == 10:
+        first_result = 0
+    cpf += str(first_result)
+    second_result += first_result * 2
+    second_result = (second_result * 10) % 11
+    if second_result == 10:
+        second_result = 0
+    cpf += str(second_result)
     return cpf
 
 
 def generate() -> str:
-    pass
+    cpf = ""
+    for _ in range(9):
+        cpf += str(randint(0, 9))
+    cpf = __generate_verificator_digits(cpf)
+    if cpf.count(cpf[0]) != 11:
+        return cpf
+    return generate()
+
+
+def gen_generate() -> Generator:
+    while True:
+        yield generate()
+
+
+async def agen_generate() -> AsyncGenerator:
+    while True:
+        yield generate()
 
 
 CPF = Annotated[str, AfterValidator(validate)]
--- tests/cpf.py
+++ tests/cpf.py
@@ -1,16 +1,16 @@
-from doctest import master
-
 from br_docs import cpf
 
 
-cpfs = [
-    "11111111111",
-    "28412832103",
-    "21037322002"
-]
+cpfs = ["11111111111", "28412832103", "21037322002"]
 
 for c in cpfs:
     print(c)
     c = cpf.mask(c)
     print(cpf.mask(c))
     print(cpf.is_valid(c))
+    new_cpf = cpf.generate()
+    print(new_cpf)
+    print(cpf.is_valid(new_cpf))
+    for i in cpf.gen_generate():
+        print(i)
+        print(cpf.is_valid(i))
